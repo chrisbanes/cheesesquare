51b5182f14026f0ab97a47a0d4b46991dc6eaed4
Zhang Hai <dreaming.in.code.zh@gmail.com>
2015-10-10 01:40:41 +0800

    You cannot hide an anchoring FAB, neither using visibility nor alpha, neither in XML nor in Java.

--- app/src/main/java/com/support/android/designlibdemo/CheeseDetailActivity.java
+++ app/src/main/java/com/support/android/designlibdemo/CheeseDetailActivity.java
@@ -19,10 +19,12 @@ package com.support.android.designlibdemo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -48,6 +50,10 @@ public class CheeseDetailActivity extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle(cheeseName);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setAlpha(0f);
+        fab.setVisibility(View.GONE);
 
         loadBackdrop();
     }
