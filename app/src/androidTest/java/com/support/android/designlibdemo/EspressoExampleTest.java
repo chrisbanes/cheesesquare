804f3b471fdd223e9385ee3c66aaa9b57139cccd
piotrek1543 <piotrekert90@gmail.com>
2015-11-13 14:00:02 +0100

    Adding Espresso UI test framework dependencies.

---
+++ app/src/androidTest/java/com/support/android/designlibdemo/EspressoExampleTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.support.android.designlibdemo;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EspressoExampleTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void checkIfAppNameIsDisplayed() {
+        onView(withText(R.string.app_name)).check(matches(isDisplayed()));
+    }
+}
